d93f411ff1f40b8b757e7a0448ad7e6aef33f209
Ann Novakowski <annhnova@gmail.com>
2017-05-03 20:57:39 -0500

    Add user model specs (failing)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home' => 'pages#home'
   get 'about' => 'pages#about'
+
   resources :lists, only: [] do
     resources :items, only: [:create, :destroy]
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  it { should validate_uniqueness_of(:name) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email)}
+  it { should have_many(:items) }
+  it { should have_many(:lists) }
 end
