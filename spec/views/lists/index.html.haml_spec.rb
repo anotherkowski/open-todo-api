ec6bb9b20fdc5719fb1bff2c8bc44d40612e18ac
Ann Novakowski <annhnova@gmail.com>
2017-05-03 20:29:31 -0500

    Delete unneeded view specs

--- spec/views/lists/index.html.haml_spec.rb
+++ spec/views/lists/index.html.haml_spec.rb
@@ -1,19 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "lists/index", type: :view do
-  before(:each) do
-    assign(:lists, [
-      List.create!(
-        :title => "Title"
-      ),
-      List.create!(
-        :title => "Title"
-      )
-    ])
-  end
 
-  it "renders a list of lists" do
-    render
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-  end
 end
--- spec/views/lists/show.html.haml_spec.rb
+++ spec/views/lists/show.html.haml_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "lists/show", type: :view do
-  before(:each) do
-    @list = assign(:list, List.create!(
-      :title => "Title"
-    ))
-  end
-
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Title/)
-  end
+  
 end
