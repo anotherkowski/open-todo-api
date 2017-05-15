190fb1dc22f754ce52e0901a57343ae0787d9382
Ann Novakowski <annhnova@gmail.com>
2017-05-14 20:57:11 -0500

    Update gitignore with bash profile aliases

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :list
   belongs_to :user
+  # ActiveRecord Validations
+  validates_associated :list
+  validates_associated :user
+  validates :name, presence: true
+
 end
