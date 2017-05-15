85aab89f2b246adf363db1be562f780f6b253b7b
Ann Novakowski <annhnova@gmail.com>
2017-05-14 20:51:04 -0500

    Add to Item model spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  
+  let(:user) { User.create!(name: 'Sample User', email: 'user@example.com', password: 'password') }
+  let(:list) { List.create!(title: 'Sample list', user_id: user) }
+
+  ## Shoulda tests for title
+  it { should validate_uniqueness_of(:title) }
+  it { is_expected.to validate_length_of(:title).is_at_least(3) }
+  it { should validate_presence_of(:title) }
+  ## Shoulda tests for list id
+  it { should validate_presence_of(:list_id)}
+  # Shoulda tests for active record associations / nested resources
+  it { should belong_to(:list) }
+  it { should belong_to(:user) }
+
+  describe 'attributes' do
+     it 'should have title attribute' do
+       expect(list).to have_attributes(title: 'Sample list', user_id: user.id)
+     end
+   end
+   describe 'invalid list' do
+     let(:list_with_invalid_title) { List.new(title: '', user_id: user.id) }
+     let(:list_with_invalid_user) { List.new(title: 'Sample list', user_id: '') }
+
+     it 'should be an invalid list due to blank title' do
+       expect(list_with_invalid_title).to_not be_valid
+     end
+     it 'should be an invalid list due to blank user' do
+       expect(list_with_invalid_user).to_not be_valid
+     end
+   end
 end
