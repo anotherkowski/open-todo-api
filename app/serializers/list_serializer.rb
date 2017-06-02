09c62b5dc50f2e9ca2c7653a2f4d495e70507365
A. Novakowski <anotherkowski@users.noreply.github.com>
2017-06-01 21:51:51 -0400

    Update list_serializer.rb

--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -1,6 +1,5 @@
 class ListSerializer < ActiveModel::Serializer
   attributes :id, :title, :user_id, :public, :created_at
-  has_many :items
 
   def created_at
     object.created_at.strftime('%B %d, %Y')
