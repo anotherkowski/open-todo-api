8da032e57c7343c907b84f7e38eb00275431bc8e
A. Novakowski <anotherkowski@users.noreply.github.com>
2017-06-01 22:14:23 -0400

    Render json in API users controller create method

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,6 +6,12 @@ class Api::UsersController < ApiController
   end
   
   def create
+    user = User.new(user_params)
+     if user.save
+       render json: user
+     else
+       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+     end
   end
 
    private
