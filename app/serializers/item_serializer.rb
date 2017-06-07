8fc29008dc3176806365af1134b39cbf27c917fa
A. Novakowski <anotherkowski@users.noreply.github.com>
2017-06-07 16:52:43 -0500

    Update item_serializer.rb

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,7 +1,3 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :title, :list_id, :user_id, :created_at
-
-  def created_at
-    object.created_at.strftime('%B %d, %Y')
-  end
 end
