cd29748a69b4e915bf26d6d1d18546f45b8156d1
Anotherkowski <annhnova@gmail.com>
2017-06-28 15:34:36 -0400

    Add api lists index route

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     resources :users do
       resources :lists
     end
-    resources :lists, only: [] do
+    resources :lists, only: [:index] do
       resources :items, only: [:create]
     end
     resources :items, only: [:destroy, :index]
